51679c398d2eb839470f4abae887ceeb8d144654
Jack Mahoney (arch) <jacksmahoney@gmail.com>
2025-02-07 01:52:27 -0800

    options

--- nvim/lua/jvim/options.lua
+++ nvim/lua/jvim/options.lua
@@ -1,2 +1,19 @@
 -- Enable line numbers
 vim.opt.number = true
+
+-- Tab settings
+vim.opt.tabstop = 4        -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 4     -- Number of spaces for auto-indent
+vim.opt.expandtab = true   -- Convert tabs to spaces
+vim.opt.softtabstop = 4    -- Number of spaces a tab counts for during editing
+
+-- Show invisible characters
+vim.opt.list = true
+vim.opt.listchars = {
+    tab = '→ ',
+    lead = '·',
+    trail = '·',
+    extends = '▸',
+    precedes = '◂',
+    nbsp = '␣',
+}
