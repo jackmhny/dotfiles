54eb5a8e4760b69329e161f06a1044111e1e50ec
Jack Mahoney <jacksmahoney@gmail.com>
2025-02-10 17:25:15 -0800

    (nvim) add aerial plugin

---
+++ nvim/lua/jvim/plugins/aerial.lua
@@ -0,0 +1,25 @@
+return {
+  "stevearc/aerial.nvim",
+  config = function()
+    require('aerial').setup({
+      -- Priority list of preferred backends for aerial
+      backends = { "treesitter", "lsp", "markdown", "man" },
+      
+      layout = {
+        -- These control the width of the aerial window
+        max_width = 40,
+        min_width = 10,
+        
+        -- Key to toggle aerial
+        default_direction = "right",
+      },
+      
+      -- Keymaps in aerial window
+      keymaps = {
+        ["<CR>"] = "actions.jump",
+        ["<C-v>"] = "actions.jump_vsplit",
+        ["<C-s>"] = "actions.jump_split",
+      },
+    })
+  end,
+}
