4fe796a23b38712dded28bf76efbb33a1f5aecdb
Jack Mahoney <jacksmahoney@gmail.com>
2025-03-28 05:46:55 -0700

    (waybar) rename to jsonc remove autocmd

--- nvim/lua/jvim/autocmds.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*waybar/config" },  -- More general pattern
-  command = "set ft=jsonc",        -- Using command instead of callback
-})
