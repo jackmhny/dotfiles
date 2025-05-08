33da78f58692768cad8627163c8adf4f4bd31464
Jack Mahoney <jacksmahoney@gmail.com>
2025-05-07 23:09:50 -0700

    (nvim) add treesitter plugin

---
+++ nvim/lua/jvim/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        keymaps = {
+                            ["at"] = "@tag.outer",
+                            ["it"] = "@tag.inner",
+                        },
+                    },
+                    move = {
+                        enable = true,
+                        goto_next_start = {
+                            ["]t"] = "@tag.outer",
+                        },
+                        goto_previous_start = {
+                            ["[t"] = "@tag.outer",
+                        },
+                    },
+                },
+            })
+        end
+    }
+}
